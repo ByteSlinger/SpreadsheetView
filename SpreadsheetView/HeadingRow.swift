8676812f90544c88a6a2d6af2856a15ccac009ac
Michael S. Hearn <michael@byteslinger.net>
2017-08-22 18:49:52 -0700

    Initial Checkin
    Framework and accompanying Demo

---
+++ SpreadsheetView/HeadingRow.swift
@@ -0,0 +1,105 @@
+//
+//  HeadingRow.swift
+//  SpreadsheetView
+//
+//  Created by ByteSlinger on 2017-08-10.
+//  Copyright © 2017 ByteSlinger. All rights reserved.
+//
+
+import UIKit
+
+let HEADING_ROW_ID = "HeadingRow"
+
+class HeadingRow: UICollectionView, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    @IBOutlet weak var spreadsheetView: SpreadsheetView!
+    
+    let reuseIdentifier = HEADING_ROW_ID
+    
+    private var isScrolling = false             // semaphore to prevent infinite scroll loops
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        // so we don't have to bother setting them in the storyboard
+        self.dataSource = self
+        self.delegate = self
+        
+        self.isScrollEnabled = false        // allow TableView to handle scrolling
+        self.allowsMultipleSelection = true
+        
+        // turn off all bouncing so scrolling/syncing works properly
+        self.bounces = false
+        self.alwaysBounceVertical = false
+        self.alwaysBounceHorizontal = false
+     }
+    
+    // sync the heading row with the passed contentOffset
+    func scrollTo(_ offset: CGPoint) {
+        if (self.isScrolling == false) {
+            self.isScrolling = true
+
+            if (offset.x != self.contentOffset.x) {
+                self.contentOffset.x = offset.x
+            }
+            
+            self.reloadData()
+            
+            self.isScrolling = false
+        }
+    }
+    
+    // scroll the CollectionView to the passed item (column)
+    func scrollToCell(_ item: Int) {
+        let indexPath = IndexPath(item: item, section: 0)
+        
+        self.scrollToItem(at: indexPath, at: .left, animated: false)
+    }
+    
+    // MARK: - UICollectionViewDelegateFlowLayout protocol
+    
+    // return the size of the requested cell
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize
+    {
+        let height = collectionView.frame.height
+        let width = self.spreadsheetView.getColumnWidth(forCol: indexPath.row)
+        
+        return CGSize(width: width, height: height)
+    }
+    
+    // MARK: - UICollectionViewDataSource protocol
+    
+    // tell the collection view how many cells to make
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.spreadsheetView.numDataCols()
+    }
+    
+    // make a cell for each cell index path
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        // get a reference to our storyboard cell
+        let tableCell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! TableCell
+        
+        return tableCell
+    }
+    
+    // setup the cell accordingly
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        let tableCell = cell as! TableCell
+        let row = collectionView.tag
+        let col = indexPath.row
+
+        let value = self.spreadsheetView.getColumnHeading(col: indexPath.row)
+        
+        tableCell.setup(row: row, col: col, value: value, heading: true, selected: false, adjustWidth: true)
+    }
+    
+    // MARK: - UICollectionViewDelegate protocol
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        self.spreadsheetView.setColumnSelected(col: indexPath.row, selected: true)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        self.spreadsheetView.setColumnSelected(col: indexPath.row, selected: false)
+    }
+
+}
---
+++ SpreadsheetViewDemo/SpreadsheetViewDemo/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  SpreadSheetView
+//
+//  The view controller contains the SpreadSheetView.
+//
+//  Created by ByteSlinger on 2017-08-10.
+//  Copyright © 2017 ByteSlinger. All rights reserved.
+//
+import UIKit
+import SpreadsheetView
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var spreadsheetView: SpreadsheetView!
+
+    // despite the Info.plist settings, this is required to get upside down orientation to work
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .all;
+    }
+    
+    override func viewDidLoad() {
+        //print("ViewController.viewDidLoad()")
+        
+
+        // demonstrate how to override whatever was in the Storyboard
+        self.spreadsheetView.maxCellWidth = 80.0
+        
+        self.spreadsheetView.rowBackgroundColor = UIColor.black
+        self.spreadsheetView.rowAlternateColor = UIColor.black.withAlphaComponent(0.8)
+        self.spreadsheetView.dataLabelHighlightedBackgroundColor = UIColor.blue.withAlphaComponent(0.5)
+        self.spreadsheetView.dataLabelHighlightedTextColor = UIColor.yellow
+        self.spreadsheetView.dataLabelFontSize = 16.0
+        self.spreadsheetView.dataLabelFontColor = UIColor.white
+        self.spreadsheetView.headingLabelFontSize = 14.0
+        self.spreadsheetView.headingLabelFontColor = UIColor.yellow
+        self.spreadsheetView.headingBackgroundColor = UIColor.darkGray
+        
+        // to make these things take effect in TableView and TableCell
+        self.spreadsheetView.overrideDefaults()
+        
+        // use a random array
+        let data = Data(random: true, rows: 100, cols: 28, headingRow: true, headingColumn: true)
+        
+        self.spreadsheetView.setDataSource(dataSource: data, firstDataRowIsHeading: true, firstDataColumnIsHeading: true)
+    }
+}
